d072c208bf5688979b30a8fe6d6a2ea540838b44
zahrabateninia <zahrabatenin@gmail.com>
2025-07-15 22:12:18 +0330

    Define states of the game and render conditionally based on game stage(loading, start or game)

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,54 @@
 import { useEffect, useState } from 'react';
 import LoadingPage from './pages/LoadingPage';
 import StartPage from './pages/StartPage';
+import PlayGamePage from './pages/playGamePage';
 import charactersData from './data/characters.json'; 
+
+// we have 3 pages so three stages 
+const STAGES ={ 
+  'LOADING': 'loading',
+  'START': 'start',
+  'PLAY': 'play',
+}
 
 function App() {
   const [characters, setCharacters] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [stage, setStage] = useState(STAGES.LOADING);
+  const [difficulty, setDifficulty] = useState(null)
 
+  // Simulate fetching data
   useEffect(() => {
-    // Simulate async data fetch
-    const loadCharacters = () => {
-      setTimeout(() => {
-        setCharacters(charactersData);
-        setIsLoading(false);
-      }, 1500); // simulate network delay
-    };
+    const timeout = setTimeout(() => {
+      setCharacters(charactersData)
+      setStage(STAGES.START)
+    }, 2000)
 
-    loadCharacters();
-  }, []);
+    return () => clearTimeout(timeout)
+  }, [])
+
+  
+  const handleStartGame = (selectedDifficulty) => {
+    setDifficulty(selectedDifficulty)
+    setStage(STAGES.GAME)
+  }
+
+  // render conditionally
+  const renderStage = () => {
+    switch(stage){
+      case STAGES.LOADING: 
+        return <LoadingPage />
+      case STAGES.START:
+        return <StartPage startGame={handleStartGame} />
+      case STAGES.PLAY:
+        return <PlayGamePage difficulty={difficulty} characters={characters} />
+      default:
+        return null;
+    }
+  }
 
   return (
     <>
-      {isLoading ? <LoadingPage /> : <StartPage characters={characters} />}
+      {renderStage()}
     </>
   );
 }
