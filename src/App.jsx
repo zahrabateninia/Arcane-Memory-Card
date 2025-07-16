e05bfce57a7ecb98b8682f4b84d914d0b46e6619
zahrabateninia <zahrabatenin@gmail.com>
2025-07-16 21:47:09 +0330

    Add restart logic and fix typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
 import { useEffect, useState } from 'react';
 import LoadingPage from './pages/LoadingPage';
 import StartPage from './pages/StartPage';
-import PlayGamePage from './pages/playGamePage';
+import PlayGamePage from './pages/PlayGamePage';
 import charactersData from './data/characters.json'; 
 
-// we have 3 pages so three stages 
-const STAGES ={ 
-  'LOADING': 'loading',
-  'START': 'start',
-  'PLAY': 'play',
-}
+const STAGES = { 
+  LOADING: 'loading',
+  START: 'start',
+  PLAY: 'play',
+};
 
 function App() {
   const [characters, setCharacters] = useState([]);
   const [stage, setStage] = useState(STAGES.LOADING);
-  const [difficulty, setDifficulty] = useState(null)
+  const [difficulty, setDifficulty] = useState(null);
 
-  // Simulate fetching data
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setCharacters(charactersData)
-      setStage(STAGES.START)
-    }, 2000)
+      setCharacters(charactersData);
+      setStage(STAGES.START);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, []);
 
-    return () => clearTimeout(timeout)
-  }, [])
-
-  
   const handleStartGame = (selectedDifficulty) => {
-    setDifficulty(selectedDifficulty)
-    setStage(STAGES.GAME)
-  }
+    setDifficulty(selectedDifficulty);
+    setStage(STAGES.PLAY); 
+  };
 
-  // render conditionally
+  const handleRestartGame = () => {
+    setDifficulty(null);
+    setStage(STAGES.START);
+  };
+
   const renderStage = () => {
     switch(stage){
       case STAGES.LOADING: 
@@ -40,7 +40,11 @@ function App() {
       case STAGES.START:
         return <StartPage startGame={handleStartGame} />
       case STAGES.PLAY:
-        return <PlayGamePage difficulty={difficulty} characters={characters} />
+        return <PlayGamePage 
+                  difficulty={difficulty} 
+                  characters={characters}
+                  restartGame={handleRestartGame}
+               />
       default:
         return null;
     }
