b38fdb8b313cf9b1530c35c237a3db37ad068223
zahrabateninia <zahrabatenin@gmail.com>
2025-07-17 21:41:35 +0330

    Implement GameOver component logic

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,12 +1,24 @@
-// Dialog overlay shown when game ends
-import React from 'react'
+import React from 'react';
+import './GameOver.css';
+import winImg from '../assets/img/powderEkkoDance.webp';
+import loseImg from '../assets/img/viCrying.webp';
 
-const GameOver = () => {
+const GameOver = ({ didWin, onRestart }) => {
   return (
-    <div>
-      
+    <div className="game-over-overlay">
+      <div className="game-over-dialog">
+        <img
+          src={didWin ? winImg : loseImg}
+          alt={didWin ? 'Victory' : 'Defeat'}
+          className="game-over-image"
+        />
+        <h2>{didWin ? 'You Win!' : 'You Lose!'}</h2>
+        <button className="restart-btn" onClick={onRestart}>
+          Restart Game
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default GameOver
+export default GameOver;
